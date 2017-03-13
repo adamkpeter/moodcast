97837cf3a6c62b8fbf948ac6d3c9f15cfe9fd964
adamkpeter <adamkpeter@gmail.com>
2017-03-13 01:53:54 -0700

    Minor changes to css and functionality with retrieving from backend

--- www/js/play.js
+++ www/js/play.js
@@ -1,7 +1,31 @@
-app.controller('playController', function ($scope, $state, Spotify, $ionicModal, localStorageService, weatherService) { //store the entities name in a variable var taskData = 'task';
+app.controller('playController', function ($scope, $state, Spotify, $ionicModal, $http, localStorageService, weatherService) { //store the entities name in a variable var taskData = 'task';
 
   var audioObject;// = new Audio("https://p.scdn.co/mp3-preview/70571de42bc2e10e2e12832416beb193b364787e?cid=null");
   //audioObject.play();
+
+  $scope.contentLoaded = false;
+
+  $http({
+    url:"http://ec2-52-37-193-231.us-west-2.compute.amazonaws.com/?ezreal=2ih4temylyfe4&weather=31&time=14&liked=1&disliked=1&preferences=electronic",
+    method: 'GET',
+    transformResponse: [function (data) {
+      console.log(data);
+      return data;
+    }]
+  }).then(function(res){
+    console.log(res);
+  }, function(err){
+    console.log(err);
+  });
+
+  // $http.get(
+  //   "http://ec2-52-37-193-231.us-west-2.compute.amazonaws.com/?ezreal=2ih4temylyfe4&weather=sun&time=day&liked=1&disliked=1&preferences=electronic|chill").then(
+  //   function(res){
+  //     console.log("help");
+  //     console.log(res);
+  //   }, function(err){
+  //     console.log(err);
+  //   });
 
 
   weatherService.getLatAndLong().then(function(res){
@@ -16,6 +40,7 @@ app.controller('playController', function ($scope, $state, Spotify, $ionicModal,
     })
   }, function(err){
     console.log(err);
+    getSong();
   });
 
 
@@ -25,7 +50,8 @@ app.controller('playController', function ($scope, $state, Spotify, $ionicModal,
       console.log($scope.track);
       $scope.source = $scope.track.preview_url;
 
-      angular.element(document.querySelector('#loading')).html = "";
+      $scope.contentLoaded = true;
+      //angular.element(document.querySelector('#loading')).html = "";
       //audioObject.play();
     });
   };
